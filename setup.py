05d9021ecd0d12913a9484ca668d94df63d3bfd3
george <george.winstone@gmail.com>
2017-04-28 18:12:50 +0100

    asasas

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'figures2canvas',
+  packages = ['figures2canvas'], # this must be the same as the name above
+  version = '0.1',
+  description = 'merges currently open figures',
+  author = 'George Winstone',
+  author_email = 'george.winstone@gmail.com',
+  url = 'https://github.com/georgewinstone/figures2canvas', # use the URL to the github repo
+  download_url = 'https://github.com/georgewinstone/figures2canvas/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['plotting', 'figures', 'lazyness'], # arbitrary keywords
+  classifiers = [],
+)
